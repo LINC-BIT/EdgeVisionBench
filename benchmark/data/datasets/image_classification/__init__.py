e73505b7437be095bf7473fd1b568f3c91c917dd
EdgeSys <73862727+LINC-BIT@users.noreply.github.com>
2024-10-28 17:07:54 +0800

    Update __init__.py

--- benchmark/data/datasets/image_classification/__init__.py
+++ benchmark/data/datasets/image_classification/__init__.py
@@ -9,19 +9,4 @@ from .imagenet_a import ImageNetA
 from .caltech256 import Caltech256
 from .domainnet_real import DomainNetReal
 
-from .cifar10LT import CIFAR10LT_001,CIFAR10LT_0005,CIFAR10LT_002,CIFAR10LT_005
-from .svhnLT import SVHNLT_001,SVHNLT_002,SVHNLT_005,SVHNLT_0005
 
-from .stl10LT import STL10LT_001,STL10LT_002,STL10LT_005,STL10LT_0005
-from .mnistLT import MNISTLT_0005,MNISTLT_001,MNISTLT_002,MNISTLT_005
-from .uspsLT import USPSLT_001,USPSLT_002,USPSLT_005,USPSLT_0005
-from .cifar10C import CIFAR10C_001
-
-
-# from ....data.datasets.registery import static_dataset_registery
-#
-# print(static_dataset_registery.keys())
-
-
-
-longlabel=[]
